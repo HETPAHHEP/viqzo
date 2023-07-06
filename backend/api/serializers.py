159d298a5410ce5b3dfcb9dd5ee488e1f2fad945
HETPAHHEP <putinisathief@pm.me>
2023-07-06 15:45:17 +0300

    feat: add alias link functionality
    You can create custom links and get them. Unable to create a link with an already existing name

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
 
 from core.enums import Limits
-from links.models import ShortLink
+from links.models import ShortLink, AliasShortLink
+from links import validators
 
 from .services.url_short_logic import LinkHash
 
 
-class LinkShowSerializer(serializers.ModelSerializer):
+class ShortLinkShowSerializer(serializers.ModelSerializer):
     """Сериализатор для показа ссылок"""
+
+    class Meta:
+        model = ShortLink
+        fields = [
+            'original_link', 'short_url', 'clicks_count',
+            'last_clicked_at', 'is_active', 'created_at'
+        ]
+
+
+class AliasLinkShowSerializer(serializers.ModelSerializer):
+    """Сериализатор для показа ссылок"""
+    short_url = serializers.CharField(source='alias')
 
     class Meta:
         model = ShortLink
@@ -20,17 +34,45 @@ class LinkShowSerializer(serializers.ModelSerializer):
 class LinkWriteSerializer(serializers.Serializer):
     """Сериализатор для записи ссылок"""
     original_link = serializers.URLField(
-        max_length=Limits.MAX_LEN_ORIGINAL_LINK
+        max_length=Limits.MAX_LEN_ORIGINAL_LINK.value
     )
+    alias = serializers.CharField(
+        max_length=Limits.MAX_LEN_ALIAS_CODE.value,
+        min_length=Limits.MIN_LEN_ALIAS_CODE.value,
+        validators=[validators.AliasShortURLValidator],
+        required=False
+    )
+
+    def validate(self, attrs):
+        alias = attrs.get('alias')
+
+        if alias:
+
+            if AliasShortLink.objects.filter(alias=alias).exists():
+                raise serializers.ValidationError({
+                    'alias_error': _('Данный код для ссылки уже занят.')
+                })
+
+        return attrs
 
     def create(self, validated_data) -> tuple[ShortLink, bool]:
         """Создание сокращенной ссылки для оригинальной"""
         original_link = validated_data.get('original_link')
-        url, created = ShortLink.objects.get_or_create(
-            original_link=original_link
-        )
+        alias = validated_data.get('alias')
 
-        if created:
+        if alias:
+            url = AliasShortLink.objects.create(
+                original_link=original_link,
+                alias=alias
+            )
+            created = True
+
+        else:
+            url, created = ShortLink.objects.get_or_create(
+                original_link=original_link
+            )
+
+        if created and not alias:
             while not url.short_url:
                 # проверяем, не занят ли короткий код и присваиваем его ссылке
                 short_code = LinkHash().get_short_code()
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,46 +3,71 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from links.models import ShortLink
+from links.models import ShortLink, AliasShortLink
 
-from .serializers import LinkShowSerializer, LinkWriteSerializer
+from .serializers import ShortLinkShowSerializer, LinkWriteSerializer, AliasLinkShowSerializer
 
 
-class GetShortLinkView(APIView):
+class BaseShortLinkView(APIView):
+    """Базовый View для короткой ссылки"""
+
+    @staticmethod
+    def get_serializer(instance):
+        """Для коротких и пользовательских ссылок"""
+        if isinstance(instance, ShortLink):
+            return ShortLinkShowSerializer
+        return AliasLinkShowSerializer
+
+
+class GetShortLinkView(BaseShortLinkView):
     """View для получения короткой ссылки"""
 
     def get(self, request, short_url) -> Response:
-        try:
-            short_link = ShortLink.objects.get(short_url=short_url)
-            short_link.clicks_count += 1
-            short_link.save()
+        short_link = ShortLink.objects.filter(short_url=short_url)
 
-            return Response(
-                LinkShowSerializer(instance=short_link).data,
-                status=status.HTTP_200_OK
-            )
-        except ShortLink.DoesNotExist:
-            return Response(
-                {"error": _("Ссылка не найдена.")},
-                status=status.HTTP_404_NOT_FOUND
-            )
+        if not short_link:
+            short_link = AliasShortLink.objects.filter(alias=short_url)
+
+            if not short_link:
+                return Response(
+                    {"error": _("Ссылка не найдена.")},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+        short_link = short_link.first()
+        short_link.clicks_count += 1
+        short_link.save()
+
+        serializer = self.get_serializer(short_link)
+
+        return Response(
+            serializer(instance=short_link).data,
+            status=status.HTTP_200_OK
+        )
 
 
-class CreateShortLinkView(APIView):
+class CreateShortLinkView(BaseShortLinkView):
     """View для создания (или получения) короткой ссылки"""
+    serializer_create = LinkWriteSerializer
 
     def post(self, request) -> Response:
-        serializer = LinkWriteSerializer(data=request.data)
+        """Создание или получения уже созданных коротких ссылок"""
+
+        serializer = self.serializer_create(data=request.data)
         if serializer.is_valid():
             instance, created = serializer.save()
+            serializer = self.get_serializer(instance)
+
             if created:
                 return Response(
-                    LinkShowSerializer(instance=instance).data,
+                    serializer(instance=instance).data,
                     status=status.HTTP_201_CREATED
                 )
+
             # ссылка уже существует
             return Response(
-                LinkShowSerializer(instance=instance).data,
+                serializer(instance=instance).data,
                 status=status.HTTP_200_OK
             )
+        # ошибки при создании
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
