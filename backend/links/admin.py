564d3be2dd6e36bd764878dfce448d16586f4527
HETPAHHEP <putinisathief@pm.me>
2023-07-07 23:48:58 +0300

    feat: add alias links to admin page

--- backend/links/admin.py
+++ backend/links/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from .models import ShortLink
+from .models import ShortLink, AliasShortLink
 
 
 @admin.register(ShortLink)
 class ShortURL(admin.ModelAdmin):
-    list_display = ['original_link', 'short_url', 'created_at']
+    list_display = ['original_link', 'short_url', 'clicks_count', 'created_at']
+    search_fields = ['original_link', 'short_url']
+    date_hierarchy = 'created_at'
+
+
+@admin.register(AliasShortLink)
+class AliasShortURL(admin.ModelAdmin):
+    list_display = ['original_link', 'alias', 'clicks_count', 'created_at']
     search_fields = ['original_link', 'short_url']
     date_hierarchy = 'created_at'
