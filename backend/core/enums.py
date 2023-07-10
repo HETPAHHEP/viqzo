c0e551a1ffd11cb5f048376dadd7321210229f7f
HETPAHHEP <putinisathief@pm.me>
2023-07-10 19:52:07 +0300

    feat: update limits

--- backend/core/enums.py
+++ backend/core/enums.py
@@ -10,3 +10,15 @@ class Limits(IntEnum):
     MIN_LEN_ALIAS_CODE = 5
     # Максимальная длина пользовательской ссылки
     MAX_LEN_ALIAS_CODE = 30
+    # Максимальная длинна названия группы
+    MAX_LEN_GROUP_NAME = 30
+    # Максимальное количество коротких ссылок в группе
+    MAX_LINKS_GROUP_AMOUNT = 100
+    # Максимальное количество групп
+    MAX_GROUPS_AMOUNT = 150
+    # Максимальная длинна названия компании
+    MAX_LEN_CAMPAIGN_NAME = 40
+    # Максимальное количество групп в компании
+    MAX_GROUPS_CAMPAIGN_AMOUNT = 10
+    # Максимальное количество компаний
+    MAX_CAMPAIGNS_AMOUNT = 10
