702bfb5d01efce4037683b29c27ae3f1bdf0a25d
HETPAHHEP <putinisathief@pm.me>
2024-03-05 21:53:35 +0300

    docs: add autogenerated openapi lib
    Need to customize openapi generation

--- backend/viqzo/dev-settings.py
+++ backend/viqzo/dev-settings.py
@@ -32,3 +32,34 @@ EMAIL_FILE_PATH = BASE_DIR / 'api' / 'sent_emails'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
 EMAIL_ADMIN = EMAIL_HOST_USER
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'django_filters',
+
+    'drf_spectacular',  # OpenAPI docs
+
+    'api',
+    'links',
+]
+
+REST_FRAMEWORK.update(
+    {'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'}
+)
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Viqzo API',
+    'DESCRIPTION': 'Сервис сокращенный ссылок',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
