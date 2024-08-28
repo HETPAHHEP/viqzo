32b6c456925bce03dffd23ee8ca1675e6ee5ea67
Ilya Kalinin <putinisathief@pm.me>
2024-08-28 15:39:08 +0300

    feat(admin): add color page and update app name

--- backend/links/admin.py
+++ backend/links/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
-from .models import ShortLink, UserGroup
+from .models import Color, ShortLink, UserGroup
 
 
 @admin.register(ShortLink)
@@ -28,6 +28,22 @@ class UserGroupAdmin(admin.ModelAdmin):
         """Демонстрация цвета в панели"""
         return format_html(
             f'<span style="color: {obj.color};">{obj.color}</span>'
+        )
+
+    colored_hex.short_description = _("Показ цвета")
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "colored_hex"]
+    search_fields = ["name", "color_hex"]
+    search_help_text = "Поиск цвета по названию и HEX"
+    ordering = ["name"]
+
+    def colored_hex(self, obj):
+        """Демонстрация цвета в панели"""
+        return format_html(
+            f'<span style="color: {obj.color_hex};">{obj.color_hex}</span>'
         )
 
     colored_hex.short_description = _("Показ цвета")
--- backend/links/apps.py
+++ backend/links/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class UrlsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "links"
+    verbose_name = "Ссылки и Группы"
